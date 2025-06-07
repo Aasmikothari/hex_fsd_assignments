7f1a808915527f3a206ed1c5935c9a45888db651
Aasmi Kothari <aasmikothari@gmail.com>
2025-06-08 03:47:48 +0530

    updated security config

--- sprint_4/amazecare/src/main/java/com/casestudy/amazecare/SecurityConfig.java
+++ sprint_4/amazecare/src/main/java/com/casestudy/amazecare/SecurityConfig.java
@@ -21,12 +21,47 @@ public class SecurityConfig {
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf((csrf) -> csrf.disable()) 
-			.authorizeHttpRequests(authorize -> authorize
-					.requestMatchers("/api/user/signup").permitAll()
-					.requestMatchers("/api/user/token").authenticated()
-					.requestMatchers("/api/department/**").permitAll()
-					.anyRequest().authenticated()  
-			)
+			.authorizeHttpRequests(auth -> auth
+				    // everyone can access
+				    .requestMatchers("/api/user/signup").permitAll()
+				    .requestMatchers("/api/user/token").authenticated()
+				    .requestMatchers("/api/user/details").authenticated()
+				    .requestMatchers("/api/department/get-all").permitAll()
+
+				    // admin access
+				    .requestMatchers("/api/department/add").hasAuthority("ADMIN")
+				    .requestMatchers("/api/department/update/**").hasAuthority("ADMIN")
+				    .requestMatchers("/api/department/delete/**").hasAuthority("ADMIN")
+				    .requestMatchers("/api/doctor/add").hasAuthority("ADMIN")
+				    .requestMatchers("/api/doctor/delete/**").hasAuthority("ADMIN")
+				    .requestMatchers("/api/patient/get-all").hasAuthority("ADMIN")
+
+				    // doctor access
+				    .requestMatchers("/api/consultation/add").hasAuthority("DOCTOR")
+				    .requestMatchers("/api/prescription/add").hasAuthority("DOCTOR")
+				    .requestMatchers("/api/test/add").hasAuthority("DOCTOR")
+				    .requestMatchers("/api/medical-record/add").hasAuthority("DOCTOR")
+
+				    // patient access
+				    .requestMatchers("/api/appointment/schedule").hasAuthority("PATIENT")
+				    .requestMatchers("/api/patient/add").hasAuthority("PATIENT")
+				    .requestMatchers("/api/patient/update/**").hasAuthority("PATIENT")
+				    .requestMatchers("/api/patient/delete/**").hasAuthority("PATIENT")
+
+				    // access with admin, doctor, patient
+				    .requestMatchers("/api/doctor/get-all").hasAnyAuthority("ADMIN", "DOCTOR", "PATIENT")
+				    .requestMatchers("/api/appointment/view").hasAnyAuthority("ADMIN", "DOCTOR", "PATIENT")
+				    .requestMatchers("/api/consultation/view").hasAnyAuthority("ADMIN", "DOCTOR", "PATIENT")
+				    .requestMatchers("/api/prescription/view").hasAnyAuthority("ADMIN", "DOCTOR", "PATIENT")
+				    .requestMatchers("/api/test/view").hasAnyAuthority("ADMIN", "DOCTOR", "PATIENT")
+				    .requestMatchers("/api/medical-record/view").hasAnyAuthority("ADMIN", "DOCTOR", "PATIENT")
+				    .requestMatchers("/api/patient/get-one/**").hasAnyAuthority("ADMIN", "DOCTOR", "PATIENT")
+				    .requestMatchers("/api/doctor/update/**").hasAnyAuthority("ADMIN", "DOCTOR", "PATIENT")
+
+				    // Any other APIs must be authenticated
+				    .anyRequest().authenticated()
+				)
+			
 		 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class) 
 		 .httpBasic(Customizer.withDefaults()); //<- this activated http basic technique
 		return http.build();
